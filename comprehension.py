b9565a4cc5688983ece7a9c2248efdc798b6474e
Gangadhar-valeti <gowtham021104@gmail.com>
2025-09-01 22:00:32 +0530

    keep only api.py

--- comprehension.py
+++
@@ -1,3 +0,0 @@
-tillu=["apple","banana","cherry","pineapple"]
-tillu= [fruit.upper() for fruit in tillu]
-print(tillu)
--- object.py
+++
@@ -1,8 +0,0 @@
-from car import Car
-
-car1=Car("rr","red",2023,False)
-car2=Car("mustag","blue",2026,True)
-
-print(car1.model)      
-print(car2.year) 
-car1.stop()
